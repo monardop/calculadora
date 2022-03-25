285e5e4f5ab5e23cd06b7b8af51f4f0fc3b0dfdb
Pablo Monardo <pablomonardo@gmail.com>
2022-03-25 18:34:51 -0300

    Empece a armar la logica de la app, tengo que revisar algunos apuntes primero

---
+++ Web/app.js
@@ -0,0 +1,10 @@
+var varAnterior;
+
+function introducirNumero(nro, varAnterior){
+    alert("Buenas, apretaste un nro");
+    varAnterior += nro;
+    return varAnterior;
+}
+function operadorBasico(){
+    alert("Esto es un operador");
+}
