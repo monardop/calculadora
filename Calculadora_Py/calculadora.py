273565330ca9b2adb9807f82e87759a38b6705cb
Pablo Monardo <pablomonardo@gmail.com>
2022-04-16 09:03:07 -0300

    Estetica de la calculadora

--- Calculadora_Py/calculadora.py
+++ Calculadora_Py/calculadora.py
@@ -1,14 +1,16 @@
 import PySimpleGUI as sg
 
+sg.theme('Dark')
+sg.set_options(font = 'Arial 18')
+btn_size = (6,3)
 layout = [
-    [sg.Text('output')],
-    [sg.Button('Clear'),sg.Button('Enter')],
-    [sg.Button(7),sg.Button(8),sg.Button(9),sg.Button('*')],
-    [sg.Button(4),sg.Button(5),sg.Button(6),sg.Button('/')],
-    [sg.Button(1),sg.Button(2),sg.Button(3),sg.Button('-')],
-    [sg.Button(0),sg.Button('.'),sg.Button('+')],
+    [sg.Text('output', font = 'Arial 21')],
+    [sg.Button('Clear', expand_x= True),sg.Button('Enter', expand_x= True)],
+    [sg.Button(7, size = btn_size),sg.Button(8, size = btn_size),sg.Button(9, size = btn_size),sg.Button('*', size = btn_size)],
+    [sg.Button(4, size = btn_size),sg.Button(5, size = btn_size),sg.Button(6, size = btn_size),sg.Button('/', size = btn_size)],
+    [sg.Button(1, size = btn_size),sg.Button(2, size = btn_size),sg.Button(3, size = btn_size),sg.Button('-', size = btn_size)],
+    [sg.Button(0, expand_x= True),sg.Button('.', size = btn_size),sg.Button('+', size = btn_size)],
 ]
-
 window = sg.Window('Calculator', layout)
 
 while True:
