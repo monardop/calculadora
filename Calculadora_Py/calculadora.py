2b231c578d002fa4bceb70690e3d8a1a5085d789
Pablo Monardo <pablomonardo@gmail.com>
2022-04-16 09:59:10 -0300

    Calculadora terminada, tengo que hacer el manejo de errores

--- Calculadora_Py/calculadora.py
+++ Calculadora_Py/calculadora.py
@@ -6,12 +6,13 @@ def change_theme(theme):
     btn_size = (6,3)
     layout = [
         [sg.Text(
-            'output',
+            'Ingrese operaciones',
             font = 'Arial 21', 
             justification= "center", 
             expand_x= True, 
             pad = (15, 30),
-            right_click_menu= theme_menu)
+            right_click_menu= theme_menu,
+            key = '-TEXT-')
         ],
         [sg.Button('Clear', expand_x= True),sg.Button('Enter', expand_x= True)],
         [sg.Button(7, size = btn_size),sg.Button(8, size = btn_size),sg.Button(9, size = btn_size),sg.Button('*', size = btn_size)],
@@ -25,22 +26,39 @@ theme_menu = ['menu', ['Dark', 'DarkGray8','LightGreen3','random']]
 window = change_theme('DarkGray8')
 
 current_num = []
+operation = []
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
     if event in theme_menu[1]:
         window.close()
         window = change_theme(event)
+
     if event in ['0','1','2','3','4','5','6','7','8','9','.']:
         current_num.append(event)
         num_string = ''.join(current_num)
-        print(event)
+        window['-TEXT-'].update(num_string)
+        
+
     if event in ['+','-','/','*']:
-        print(event)
+        operation.append(''.join(current_num))
+        current_num = []
+        operation.append(event)
+        window['-TEXT-'].update('')
+
     if event == 'Enter':
-        print(event)
+        if operation != []:
+            operation.append(''.join(current_num))
+            rta = eval(''.join(operation))
+            window['-TEXT-'].update(rta)
+            operation = []
+
     if event == 'Clear':
-        print(event)
+        operation = []
+        current_num = []
+        window['-TEXT-'].update('')
+
 window.close()
